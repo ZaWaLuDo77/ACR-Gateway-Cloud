b082dc2e2c85b3fc30475fa03cd8afcea9ab6309
tienen77 <tn11.11101@droxotech.com>
2023-02-06 00:59:50 +0800

    vue three.js 0204

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import * as THREE from 'three'
+</script>
+
+<template>
+  <div>
+    <canvas id="three"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.initThree()
+  },
+  methods: {
+    initThree() {
+      const scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+			const renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+
+			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			const cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+
+			function animate() {
+				requestAnimationFrame( animate );
+
+				cube.rotation.x += 0.01;
+				cube.rotation.y += 0.01;
+
+				renderer.render( scene, camera );
+			};
+
+			animate();
+    },
+  },
+}
+</script>
+<style scoped>
+#three {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+</style>
